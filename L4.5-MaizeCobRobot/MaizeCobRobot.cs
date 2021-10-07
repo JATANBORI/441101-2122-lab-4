fefbb263f870ce84848c32751ced1a17a1b89c16
John Atanbori <j.atanbori@hull.ac.uk>
2021-10-07 20:25:03 +0100

    updated

---
+++ L4.5-MaizeCobRobot/MaizeCobRobot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace L4._5_MaizeCobRobot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maize Cob Counting Robot.");
+
+            const int maizeCobsPerMinute = 100;
+
+            //All the rest of your codes goes here
+            
+            Console.WriteLine("| {0,11} | {1,12} |", "Minutes", "Maize Cobs");
+            Console.WriteLine("| {0,11} | {1,12} |", "===========", "============");
+
+            for (int i = 10; i <= 30; i+=5)
+            {
+                float totalMaizeCobs = maizeCobsPerMinute * i;
+                Console.WriteLine("| {0,11} | {1,12} |", i, totalMaizeCobs);
+            }
+        }
+    }
+}
